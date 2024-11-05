e2667d16e6ba71528752377bf44481a1de949d7b
jake <pjake2216@gmail.com>
2024-11-05 20:01:36 +0530

    donr

--- src/component/header.js
+++ src/component/header.js
@@ -4,7 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
-import logoimg from '../../src/images/footer-logo.png';
+import logoimg from '../../src/images/logo.png';
 
 export default function Header() {
   useEffect(() => {
