2d4f312637b819e6cfd854a70791c3db463df5f1
jake <pjake2216@gmail.com>
2024-11-07 17:54:48 +0530

    DONE

--- src/App.js
+++ src/App.js
@@ -6,13 +6,11 @@ import Footer from "./component/footer";
 import Banner from "./component/banner";
 import LogoListing from "./component/logo-listing";
 import Table from "./component/table";
-// import Verified from "./component/verified";
 import About from "./component/about";
 import HowToBuy from "./component/how-to-buy";
 import Marque from "./component/Marque";
 import Roadmap from "./component/roadmap";
 import ContactAddress from "./component/contract-address";
-// import Countdown from "./component/Countdown";
 
 function App() {
   useEffect(() => {
--- src/component/contract-address.js
+++ src/component/contract-address.js
@@ -9,12 +9,12 @@ export default function ContactAddress() {
                     <div className='row'>
                         <div className='col-lg-6 text-center mb-lg-0 mb-5'>
                             <img src={GraphCharacter} alt="" className='vert-move' />
-                            <div className="tokenomics-content-right text-center">
+                            {/* <div className="tokenomics-content-right text-center">
                                 <h3 className="text-white mt-4 mb-3 text-center">Contract Address:</h3>
                                 <div className="tokenomics-code d-flex align-items-center justify-content-center">
                                     <input name="myInput" value={0x57fd3480581f72b0df1adead72b4181a52a1d7de} />
                                 </div>
-                            </div>
+                            </div> */}
                         </div>
                         <div className='col-lg-6'>
                             <img src={Graph} alt="" className='vert-move'/>
